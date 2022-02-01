a47af36af7e417b4e77bfeca65130e58d41e6b36
Andrey Poplavskii <andris838@gmail.com>
2022-02-01 22:03:54 +0300

    добавил скрипты и архив базы

---
+++ Linq2DB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq2DB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
